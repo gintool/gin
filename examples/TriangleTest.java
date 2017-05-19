3a848be1bd538fccd12d4b812f672ae3f7ff1567
david.r.white@ucl.ac.uk <davidrobertwhite@gmail.com>
2017-05-19 16:21:40 +0100

    Basic functional tests for triangle

--- examples/TriangleTest.java
+++ examples/TriangleTest.java
@@ -5,7 +5,7 @@ public class TriangleTest {
     private void checkClassification(int[][] triangles, Triangle.TriangleType expectedResult) {
         for (int[] triangle: triangles) {
             Triangle.TriangleType triangleType = Triangle.classifyTriangle(triangle[0], triangle[1], triangle[2]);
-            assertEquals(triangleType, expectedResult);
+            assertEquals(expectedResult, triangleType);
         }
     }
 
@@ -17,23 +17,20 @@ public class TriangleTest {
 
     @org.junit.Test
     public void testEqualateralTriangles() throws Exception {
-
-        int[][] equalateralTriangles = {{1, 1, 1}, {100, 100, 100}, {99, 99, 99},
-                {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE,},
-                {Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE}};
-
+        int[][] equalateralTriangles = {{1, 1, 1}, {100, 100, 100}, {99, 99, 99}};
         checkClassification(equalateralTriangles, Triangle.TriangleType.EQUALATERAL);
-
     }
 
     @org.junit.Test
     public void testIsocelesTriangles() throws Exception {
-
+        int[][] isocelesTriangles = {{100, 90, 90}, {1000, 900, 900}, {3,2,2}, {30,16,16}};
+        checkClassification(isocelesTriangles, Triangle.TriangleType.ISOCELES);
     }
 
     @org.junit.Test
     public void testScaleneTriangles() throws Exception {
-
+        int[][] scaleneTriangles = {{5, 4, 3}, {1000, 900, 101}, {3,20,21}, {999, 501, 600}};
+        checkClassification(scaleneTriangles, Triangle.TriangleType.SCALENE);
     }
 
 }

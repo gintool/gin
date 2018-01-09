d69005f978e156a0a30f41b631b33255dd966704
David R. White <david.r.white@ucl.ac.uk>
2018-01-09 17:53:19 +0000

    Move example to input directory

--- input/TriangleTest.java
+++ input/TriangleTest.java
@@ -1,8 +1,11 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class TriangleTest {
 
-    @org.junit.Test
+    //@org.junit.Test
+    @Test
     public void simpleTest() {
         Triangle triangle = new Triangle();
         triangle.simple();

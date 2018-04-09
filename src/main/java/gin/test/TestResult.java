12f1993baf0e67367de4c211fdebf63ea6cbfefa
David R. White <d.r.white@sheffield.ac.uk>
2018-04-09 15:38:42 +0100

    Add missed refactored files

---
+++ src/main/java/gin/test/TestResult.java
@@ -0,0 +1,54 @@
+package gin.test;
+import org.junit.runner.Result;
+
+/**
+ * Class to hold the junitResult of running jUnit.
+ */
+public class TestResult {
+
+    String patchedProgram = "";
+    Result junitResult = null;
+
+    public String getPatchedProgram() {
+        return patchedProgram;
+    }
+
+    public Result getJunitResult() {
+        return junitResult;
+    }
+
+    public double getExecutionTime() {
+        return executionTime;
+    }
+
+    public boolean getCleanCompile() {
+        return compiled;
+    }
+
+    public boolean getValidPatch() {
+        return patchSuccess;
+    }
+
+    double executionTime = -1;
+    boolean compiled = false;
+    boolean patchSuccess = false;
+
+    public TestResult(Result result, double executionTime, boolean compiled, boolean patchedOK,
+                      String patchedProgram) {
+        this.junitResult = result;
+        this.executionTime = executionTime;
+        this.compiled = compiled;
+        this.patchSuccess = patchedOK;
+        this.patchedProgram = patchedProgram;
+    }
+
+    public String toString() {
+        boolean junitOK = false;
+        if (this.junitResult != null) {
+            junitOK = this.junitResult.wasSuccessful();;
+        }
+        return String.format("Patch Valid: %b; Compiled: %b; Time: %f; Passed: %b", this.patchSuccess,
+                this.compiled, this.executionTime, junitOK);
+    }
+
+}

ca85cdec81b3ad9d578a4dab9ee60777d53e3335
Giovani Guizzo <giovaniguizzo@gmail.com>
2022-08-24 01:23:36 +0100

    Added LocalSearchRuntime.

---
+++ src/main/java/gin/util/LocalSearchRuntime.java
@@ -0,0 +1,53 @@
+package gin.util;
+
+import gin.Patch;
+import gin.test.UnitTest;
+import gin.test.UnitTestResultSet;
+import java.io.File;
+import java.util.List;
+
+/**
+ *
+ * @author Giovani
+ */
+public class LocalSearchRuntime extends LocalSearchSimple {
+
+    public static void main(String[] args) {
+        LocalSearchRuntime sampler = new LocalSearchRuntime(args);
+        sampler.sampleMethods();
+    }
+
+    public LocalSearchRuntime(String[] args) {
+        super(args);
+    }
+
+    // Constructor used for testing
+    public LocalSearchRuntime(File projectDir, File methodFile) {
+        super(projectDir, methodFile);
+    }
+
+    /*============== Implementation of abstract methods  ==============*/
+    protected UnitTestResultSet initFitness(String className, List<UnitTest> tests, Patch origPatch) {
+        return testPatch(className, tests, origPatch);
+    }
+
+    // Calculate fitness
+    protected double fitness(UnitTestResultSet results) {
+        double fitness = Double.MAX_VALUE;
+        if (results.getCleanCompile() && results.allTestsSuccessful()) {
+            return (double) (results.totalExecutionTime() / 1000000);
+        }
+        return fitness;
+    }
+
+    // Calculate fitness threshold, for selection to the next generation
+    protected boolean fitnessThreshold(UnitTestResultSet results, double orig) {
+        return results.allTestsSuccessful();
+    }
+
+    // Compare two fitness values, newFitness better if result > 0
+    protected double compareFitness(double newFitness, double oldFitness) {
+        return oldFitness - newFitness;
+    }
+
+}
---
+++ src/main/java/gin/util/LocalSearchSimple.java
@@ -0,0 +1,185 @@
+package gin.util;
+
+import gin.Patch;
+import gin.edit.Edit;
+import gin.test.UnitTest;
+import gin.test.UnitTestResultSet;
+import org.pmw.tinylog.Logger;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * Method-based LocalSearchSimple search.
+ */
+
+public abstract class LocalSearchSimple extends GP {
+
+    // Percentage of population size to be selected during tournament selection
+    private static double tournamentPercentage = 0.2;
+    // Probability of adding an edit during uniform crossover
+    private static double mutateProbability = 0.5;
+
+    public LocalSearchSimple(String[] args) {
+        super(args);
+    }
+
+    // Constructor used for testing
+    public LocalSearchSimple(File projectDir, File methodFile) {
+        super(projectDir, methodFile);
+    }
+
+    // Whatever initialisation needs to be done for fitness calculations
+    @Override
+    protected abstract UnitTestResultSet initFitness(String className, List<UnitTest> tests, Patch origPatch);
+
+    // Calculate fitness
+    @Override
+    protected abstract double fitness(UnitTestResultSet results);
+
+    // Calculate fitness threshold, for selection to the next generation
+    @Override
+    protected abstract boolean fitnessThreshold(UnitTestResultSet results, double orig);
+
+    // Compare two fitness values, result of comparison > 0 if newFitness better than oldFitness
+    @Override
+    protected abstract double compareFitness(double newFitness, double oldFitness);
+
+    /*============== Implementation of abstract methods  ==============*/
+
+    /*====== Search ======*/
+
+    // Simple GP search (based on Simple)
+    protected void search(TargetMethod method, Patch origPatch) {
+
+        Logger.info("Runnning best-first local search.");
+
+        String className = method.getClassName();
+        String methodName = method.toString();
+        List<UnitTest> tests = method.getGinTests();
+
+        // Run original code
+        UnitTestResultSet results = initFitness(className, tests, origPatch);
+
+        // Calculate fitness and record result, including fitness improvement (currently 0)
+        double orig = fitness(results);
+        super.writePatch(results, methodName, orig, 0);
+
+        // Keep best 
+        double best = orig;
+        Patch bestPatch = origPatch;
+
+        for (int i = 1; i < indNumber; i++) {
+
+            // Add a mutation
+            Patch patch = mutate(bestPatch);
+
+            // Calculate fitness
+            results = testPatch(className, tests, patch);
+            double newFitness = fitness(results);
+            super.writePatch(results, methodName, newFitness, compareFitness(newFitness, orig));
+
+            // Check if better
+            if (compareFitness(newFitness, best) > 0) {
+                best = newFitness;
+                bestPatch = patch;
+            }
+        }
+    }
+
+    /*====== GP Operators ======*/
+
+    // Adds a random edit of the given type with equal probability among allowed types
+    protected Patch mutate(Patch oldPatch) {
+        Patch patch = oldPatch.clone();
+        patch.addRandomEditOfClasses(super.mutationRng, super.editTypes);
+        return patch;
+    }
+
+    // Tournament selection for patches
+    protected List<Patch> select(Map<Patch, Double> population, Patch origPatch, double origFitness) {
+
+        List<Patch> patches = new ArrayList<>(population.keySet());
+        if (patches.size() < super.indNumber) {
+            population.put(origPatch, origFitness);
+            while (patches.size() < super.indNumber) {
+                patches.add(origPatch);
+            }
+        }
+        List<Patch> selectedPatches = new ArrayList<>();
+
+        // Pick half of the population size
+        for (int i = 0; i < super.indNumber / 2; i++) {
+
+            Collections.shuffle(patches, super.individualRng);
+
+            // Best patch from x% randomly selected patches picked each time
+            Patch bestPatch = patches.get(0);
+            double best = population.get(bestPatch);
+            for (int j = 1; j < (super.indNumber * tournamentPercentage); j++) {
+                Patch patch = patches.get(j);
+                double fitness = population.get(patch);
+
+                if (compareFitness(fitness, best) > 0) {
+                    bestPatch = patch;
+                    best = fitness;
+                }
+            }
+
+            selectedPatches.add(bestPatch.clone());
+
+        }
+        return selectedPatches;
+    }
+
+    // Uniform crossover: patch1patch2 and patch2patch1 created, each edit added with x% probability
+    protected List<Patch> crossover(List<Patch> patches, Patch origPatch) {
+
+        List<Patch> crossedPatches = new ArrayList<>();
+
+        Collections.shuffle(patches, super.individualRng);
+        int half = patches.size() / 2;
+        for (int i = 0; i < half; i++) {
+
+            Patch parent1 = patches.get(i);
+            Patch parent2 = patches.get(i + half);
+            List<Edit> list1 = parent1.getEdits();
+            List<Edit> list2 = parent2.getEdits();
+
+            Patch child1 = origPatch.clone();
+            Patch child2 = origPatch.clone();
+
+            for (Edit edit : list1) {
+                if (super.mutationRng.nextFloat() > mutateProbability) {
+                    child1.add(edit);
+                }
+            }
+            for (Edit edit : list2) {
+                if (super.mutationRng.nextFloat() > mutateProbability) {
+                    child1.add(edit);
+                }
+                if (super.mutationRng.nextFloat() > mutateProbability) {
+                    child2.add(edit);
+                }
+            }
+            for (Edit edit : list1) {
+                if (super.mutationRng.nextFloat() > mutateProbability) {
+                    child2.add(edit);
+                }
+            }
+
+            crossedPatches.add(parent1);
+            crossedPatches.add(parent2);
+            crossedPatches.add(child1);
+            crossedPatches.add(child2);
+        }
+
+        return crossedPatches;
+    }
+
+}
+
